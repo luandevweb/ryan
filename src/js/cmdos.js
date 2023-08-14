5f887285a36c454cc7d817a7cca251148e0e7bf8
luandevweb <luandevweb@gmail.com>
2023-08-13 21:32:54 -0300

    Atualizações

--- src/js/cmdos.js
+++ src/js/cmdos.js
@@ -23,29 +23,37 @@ pegaDivTres.style.display = "block"
 }
 
 function gotowhatsapp() {
+
+
+    if(document.getElementById("nome").value === ''){
+        alert("Preencha o nome completo!")
+    }else{
+
+        var nome = document.getElementById("nome").value;
+        var acpum = document.getElementById("acpum").value;
+        var acpdois = document.getElementById("acpdois").value;
+        var acptres = document.getElementById("acptres").value;
+        var acpquatro = document.getElementById("acpquatro").value;
     
-    var nome = document.getElementById("nome").value;
-    var sobrenome = document.getElementById("sobrenome").value;
-    var acpum = document.getElementById("acpum").value;
-    var acpdois = document.getElementById("acpdois").value;
-    var acptres = document.getElementById("acptres").value;
-    var acpquatro = document.getElementById("acpquatro").value;
+       
+    
+    
+        var url = "https://wa.me/5581988533296?text=" 
+    + "Olá Mamãe, estou confirmando a minha presença no 2º aninho de Ryan : " + "%0a" 
+    + " " + "%0a"   
+    + "*Nome:* " + nome + "%0a"
+    + "*Acompanhante 01:* " + acpum + "%0a"
+    + "*Acompanhante 02:* " + acpdois + "%0a"
+    + "*Acompanhante 03:* " + acptres + "%0a"
+    + "*Acompanhante 04:* " + acpquatro + "%0a"
+    
+    
+    
+        window.open(url, '_blank').focus();
 
+    }
+    
    
-
-
-    var url = "https://wa.me/5581988533296?text=" 
-+ "Olá Mamãe, estou confirmando a minha presença no 2º aninho de Ryan : " + "%0a" 
-+ " " + "%0a"   
-+ "*Nome:* " + nome + "%0a"
-+ "*Acompanhante 01:* " + acpum + "%0a"
-+ "*Acompanhante 02:* " + acpdois + "%0a"
-+ "*Acompanhante 03:* " + acptres + "%0a"
-+ "*Acompanhante 04:* " + acpquatro + "%0a"
-
-
-
-    window.open(url, '_blank').focus();
 }
 
 
